dee9a4371ddfccc53ca4fc781a10d45cd7022a42
Shweta Bhagat <bhagatshweta0216@gmail.com>
2023-12-23 14:03:23 +0530

    implement deleteBlog

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -2,6 +2,7 @@ const Blog = require('../models/blogModel')
 const mongoose = require('mongoose')
 const objectId = mongoose.Types.ObjectId
 
+
 // @desc Get All Blogs
 // @route /api/blogs
 // @access public
@@ -75,8 +76,30 @@ const updateBlog = async (req, res, next) => {
     }
 }
 
-const deleteBlog = (req, res, next) => {
-    console.log(`delete a blog with id: ${req.params.id}`)
+
+// @desc Delete a Blog
+// @route /api/blogs/:id
+// @access public
+const deleteBlog = async (req, res, next) => {
+    try {
+        const blogID = req.params.id
+        console.log(blogID)
+        if (!objectId.isValid(blogID)) {
+            res.status(400)
+            throw new Error(`${blogID} is an invalid blog Id`)
+        }
+
+        const result = await Blog.deleteOne({ _id: blogID })
+        console.log(result) // { acknowledged: true, deletedCount: 1 }
+        if (!result.deletedCount) {
+            res.status(404)
+            throw new Error(`No blog found with id ${blogID}`)
+        }
+
+        res.status(200).json(`blog with id ${blogID} deleted successfully!`)
+    } catch (err) {
+        next(err)
+    }
 }
 
 module.exports = {
