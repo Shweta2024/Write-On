d850481156ec6b8a9143e67295d26b6abd000792
Shweta Bhagat <bhagatshweta0216@gmail.com>
2023-12-23 13:46:25 +0530

    implement updateBlog

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,5 +1,6 @@
 const Blog = require('../models/blogModel')
-
+const mongoose = require('mongoose')
+const objectId = mongoose.Types.ObjectId
 
 // @desc Get All Blogs
 // @route /api/blogs
@@ -42,8 +43,36 @@ const createBlog = async (req, res, next) => {
     }
 }
 
-const updateBlog = (req, res, next) => {
-    console.log(`update a blog with id: ${req.params.id}`)
+
+// @desc Update a Blog
+// @route /api/blogs/:id
+// @access public
+const updateBlog = async (req, res, next) => {
+    console.log('updateBlog')
+    try {
+        const blogID = req.params.id
+        console.log(blogID)
+        if (!objectId.isValid(blogID)) {
+            console.log('got invalid id')
+            res.status(400)
+            throw new Error(`${blogID} is an invalid blog Id`)
+        }
+
+        const updatedBlog = await Blog.findOneAndUpdate(
+            {_id: blogID},
+            req.body,
+            {new: true}
+        )
+
+        if (!updatedBlog) {
+            res.status(404)
+            throw new Error(`No blog found with id ${blogID}`)
+        }
+
+        res.status(200).json(updatedBlog)
+    } catch (err) {
+        next(err)
+    }
 }
 
 const deleteBlog = (req, res, next) => {
