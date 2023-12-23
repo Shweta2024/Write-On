d42e62417776ca03495a56b0f85e8172a28f84bb
Shweta Bhagat <bhagatshweta0216@gmail.com>
2023-12-23 15:01:32 +0530

    implement loginUser functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,40 @@
 const User = require('../models/userModel')
 const bcrypt = require('bcrypt')
 
-const loginUser = (req, res, next) => {
-    console.log('logged in')
+
+// function to check if user exists
+const userExists = async (email) => {
+    const user = await User.findOne({ email: email })
+    console.log(user)
+    return user
+}
+
+const loginUser = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+        if (!email || !password) {
+            res.status(400)
+            throw new Error('All fields are required!')
+        }
+
+        const user = await userExists(email)
+        if (!user) {
+            res.status(404)
+            throw new Error(`No user found with email ${email}`)
+        }
+
+        console.log(password, user.password)
+        const isMatched = await bcrypt.compare(password, user.password)
+
+        if (!isMatched) {
+            res.status(400)
+            throw new Error('Password is incorrect!')
+        }
+
+        res.status(200).json(user.name)
+    } catch (err) {
+        next(err)
+    }
 }
 
 const registerUser = async (req, res, next) => {
@@ -14,8 +46,7 @@ const registerUser = async (req, res, next) => {
         }
 
         // check if email already exists
-        const userExists = await User.findOne({ email: email })
-        if (userExists) {
+        if (await userExists(email)) {
             res.status(400)
             throw new Error(`User with email ${email} already exists!`)
         }
